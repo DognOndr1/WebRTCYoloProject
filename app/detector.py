8681b7e49003da901c644c74d86f6cdb0fee8123
Your Name <you@example.com>
2024-08-29 15:31:49 +0300

    cuda eklendi

--- app/detector.py
+++ app/detector.py
@@ -9,6 +9,7 @@ class Detector:
 
     def __post_init__(self):
         self.model = YOLO("yolov8n.pt")
+        self.model.to('cuda')
 
     def process_frame(self, frame):
         if frame is None:
